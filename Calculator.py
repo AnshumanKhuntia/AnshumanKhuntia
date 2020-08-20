c7cd73dc768cb93ded6a2fff7e976f546caee4ae
Anshuman khuntia <68916445+AnshumanKhuntia@users.noreply.github.com>
2020-08-20 15:08:12 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+num2 = int(input("Enter num2:"))
+print(5+4)
+
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+print("Your answer",num1+num2)
+
+num1 = int(input("Enter num1:"))
+print("Your answer",num1-num2)
+
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+print("Your answer",num1*num2)
+
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+print("Your answer",num1/num2)
+
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+print("Your answer",num1%num2)
